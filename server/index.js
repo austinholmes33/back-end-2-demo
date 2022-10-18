89d0a21466c85ddf5ab320a833e8d4cae3ee9260
Austin Holmes <austin.holmes4@gmail.com>
2022-10-18 12:57:32 -0600

    initial

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//endpoints
+
+const {getMovies, deleteMovie, createMovie} = require('./controller.js')
+
+app.get('/api/movies', getMovies)
+
+app.delete('/api/movies/:id', deleteMovie)
+app.post('/api/movies', createMovie)
+
+app.listen(4004, () => {
+    console.log('Up on 4004')
+})
